dbeed85ac524f60ac88f03ce8f79ed5b6b35f101
Daniel Thonon <d.thonon9@gmail.com>
2022-02-07 13:38:34 +0100

    Misc changes

--- scrapper/mte/spiders/mte_crawler.py
+++ scrapper/mte/spiders/mte_crawler.py
@@ -5,37 +5,49 @@ import scrapy
 
 class MteCrawlerSpider(scrapy.Spider):
     name = "mte_crawler"
-    allowed_domains = ["developpement-durable.gouv.fr"]
-    ## Modifier le nom de la consultation ci-dessous
-    start_urls = [
-        "http://www.consultations-publiques.developpement-durable.gouv.fr/"
-        + "projet-de-decret-pris-en-application-de-l-article-a2569.html"
-    ]
+    # custom_settings = {
+    #     'DOWNLOAD_DELAY': '1',
+    # }
 
-    _max_pages = 0  # Pour limiter le nombre de pages, si nécessaire
+    # ## Modifier le nom de la consultation ci-dessous
+    _start_url = "http://www.consultations-publiques.developpement-durable.gouv.fr/projet-de-decret-pris-en-application-de-l-article-a2569.html"
+    _max_pages = None
     _page = 0
     _p_com = re.compile(r" (\d*) commentaires")
 
+    def start_requests(self):
+        ## La première page permet de calculer le nombre de pages à télécharger
+        self.logger.info("Téléchargement de la première page")
+        yield scrapy.Request(url=self._start_url, callback=self.parse)
+
+
     def parse(self, response):
-        if self._max_pages == 0:
+        ## Au début, détermination du nombre de pages à télécharger
+        if self._page == 0:
+            ## Extraction du sous-titre de la consultation
             dateart = response.css("div.dateart::text").getall()
             nb_comment = int(self._p_com.search(dateart[0]).group(1))
-            self._max_pages = ((nb_comment - 1) // 20) + 1
+            self._max_pages = ((nb_comment - 1) // 20) + 1  # 20 commentaires par page
             self.logger.info(
                 "Commentaires : %d, pages: %d", nb_comment, self._max_pages
             )
 
+        nb_com = 0
         for ligne in response.css("div.ligne-com"):
+            nb_com += 1
             yield {
                 "sujet": ligne.css("div.titresujet::text").getall(),
                 "texte": ligne.css("div.textesujet *::text").getall(),
             }
+        self.logger.info("Extraction de %d commentaires", nb_com)
 
         if self._page < self._max_pages:
             self._page += 1
-            self.logger.info("Téléchargement page %d", self._page)
+            page = "?debut_forums=" + str(20 * self._page)
+            self.logger.info("Téléchargement page %s", page)
             yield scrapy.Request(
-                self.start_urls[0] + "?debut_forums=" + str(20 * self._page),
+                self._start_url + page,
                 callback=self.parse,
                 dont_filter=True,
             )
+
